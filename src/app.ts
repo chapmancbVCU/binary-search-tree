cd98c859ea2190af7b9f770945d5993912d4cc84
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-19 16:30:14 -0400

    Performed some manual testing of array helper class.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('foo');
+/******************************************************************************
+ * IMPORTS                                                                    *
+ *****************************************************************************/
+import { ArrayHelper } from "./ArrayHelper";
+
+//let inputArray = [5, 4, 3, 2, 1];
+let sorter = new ArrayHelper([5, 4, 3, 2, 1]);
+sorter.insertionSort();
+let newArray = sorter.getArray();
+
+console.log(sorter.toString());
