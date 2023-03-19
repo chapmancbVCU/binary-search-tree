d840b9970376835d56a36ce3da6d673521abb3af
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-19 18:45:13 -0400

    Manual testing.

--- public/app.js
+++ public/app.js
@@ -4,8 +4,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * IMPORTS                                                                    *
  *****************************************************************************/
 const ArrayHelper_1 = require("./ArrayHelper");
+const BinarySearchTree_1 = require("./BinarySearchTree");
 //let inputArray = [5, 4, 3, 2, 1];
-let sorter = new ArrayHelper_1.ArrayHelper([5, 4, 3, 2, 1]);
+let sorter = new ArrayHelper_1.ArrayHelper([6, 5, 4, 3, 2, 1]);
 sorter.insertionSort();
 let newArray = sorter.getArray();
-console.log(sorter.toString());
+let bst = new BinarySearchTree_1.BinarySearchTree();
+bst.root = bst.buildBST(newArray, 0, newArray.length - 1);
+console.log(bst.prettyPrint(bst.root));
--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@
  * IMPORTS                                                                    *
  *****************************************************************************/
 import { ArrayHelper } from "./ArrayHelper";
-
+import { BinarySearchTree } from "./BinarySearchTree";
 //let inputArray = [5, 4, 3, 2, 1];
-let sorter = new ArrayHelper([5, 4, 3, 2, 1]);
+let sorter = new ArrayHelper([ 6, 5, 4, 3, 2, 1]);
 sorter.insertionSort();
 let newArray = sorter.getArray();
 
-console.log(sorter.toString());
+let bst = new BinarySearchTree();
+bst.root = bst.buildBST(newArray, 0, newArray.length-1)!;
+console.log(bst.prettyPrint(bst.root));
