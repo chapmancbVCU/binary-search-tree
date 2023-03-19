2514ac736d7eb07b751ddce40f1c91cd9ac46858
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-19 18:46:23 -0400

    Implemented ability to create BST and print data.

--- public/BinarySearchTree.js
+++ public/BinarySearchTree.js
@@ -1,9 +1,43 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BinarySearchTree = void 0;
 class BinarySearchTree {
+    //sortedArray: number[];
+    constructor() {
+        this.prettyPrint = (node, prefix = '', isLeft = true) => {
+            if (node === null) {
+                return;
+            }
+            if (node.right !== null) {
+                this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+            }
+            console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+            if (node.left !== null) {
+                this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+            }
+        };
+        this.root = null;
+        //this.sortedArray = null!;
+    }
+    buildBST(sortedArray, start, end) {
+        // Base case
+        if (start > end) {
+            return null;
+        }
+        console.log(`start: ${start}`);
+        console.log(`end: ${end}`);
+        let mid = Math.round((start + end) / 2);
+        let node = new BSTNode(sortedArray[mid]);
+        node.left = this.buildBST(sortedArray, start, mid - 1);
+        node.right = this.buildBST(sortedArray, mid + 1, end);
+        return node;
+    }
 }
+exports.BinarySearchTree = BinarySearchTree;
 class BSTNode {
     constructor(data) {
         this.data = data;
+        console.log(this.data);
         this.left = null;
         this.right = null;
     }
--- src/BinarySearchTree.ts
+++ src/BinarySearchTree.ts
@@ -1,14 +1,50 @@
-class BinarySearchTree {
-    
+export class BinarySearchTree {
+    root: BSTNode;
+    //sortedArray: number[];
+
+    constructor() {
+        this.root = null!;
+        //this.sortedArray = null!;
+    }
+
+    buildBST(sortedArray: number[], start: number, end: number) {
+        // Base case
+        if(start > end) {
+            return null;
+        }
+
+        console.log(`start: ${start}`);
+        console.log(`end: ${end}`);
+        let mid = Math.round((start + end) / 2);
+        let node = new BSTNode(sortedArray[mid]);
+        node.left = this.buildBST(sortedArray, start, mid - 1)!;
+        node.right = this.buildBST(sortedArray, mid + 1, end)!;
+
+        return node;
+    }
+
+    prettyPrint = (node: BSTNode, prefix = '', isLeft = true) => {
+        if (node === null) {
+           return;
+        }
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
+      }
 }
 
 class BSTNode {
-    public data: Number;
+    public data: number;
     public left: BSTNode;
     public right: BSTNode;
 
     constructor(data: number) {
         this.data = data;
+        console.log(this.data);
         this.left = null!;
         this.right = null!;
     }
