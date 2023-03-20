3948c3e61bf63ff5794dff6690a5dab9b2a0c095
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-20 19:33:01 -0400

    Manual testing.

--- public/app.js
+++ public/app.js
@@ -9,8 +9,24 @@ const BinarySearchTree_1 = require("./BinarySearchTree");
 let sorter = new ArrayHelper_1.ArrayHelper([6, 5, 4, 5, 9, 3, 2, 5, 10, 1]);
 sorter.insertionSort();
 sorter.removeDuplicates();
-let newArray = sorter.getArray();
-console.log(`${newArray.toString()}\n`);
+console.log(`Output: ${sorter.toString()}\n`);
+//let newArray = sorter.getArray();
 let bst = new BinarySearchTree_1.BinarySearchTree();
-bst.root = bst.buildBST(newArray, 0, newArray.length - 1);
-console.log(bst.prettyPrint(bst.root));
+let root = bst.getRootNode();
+root = bst.buildBST(sorter.getArray(), 0, sorter.getArray().length - 1);
+console.log(bst.prettyPrint(root));
+/*var array = [1,3,2,1,3,4,5,6,7,3,5,6,4,3]
+let output: number[] = new Array();
+
+function removeDuplicates(array: number[]){
+
+for(let item of array){
+    
+    if(!output.includes(item))
+      output.push(item)
+}
+console.log(output.length);
+return output
+
+}
+console.log(removeDuplicates(sorter.getArray()));*/ 
