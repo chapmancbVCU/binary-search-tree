1badaceda18b52e519c73f855c072ae7946f5080
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-20 18:16:06 -0400

    Added test for removeDuplicates function.

--- public/ArrayHelper.test.js
+++ public/ArrayHelper.test.js
@@ -23,6 +23,44 @@ describe('#insertionSort', () => {
     });
 });
 
+describe('#removeDuplicates', () => {
+    describe('Sorted array', () => {
+        test('Remove duplicates from sorted array', () => {
+            let helper = new ArrayHelper([1, 2, 3, 3 ,5 , 3, 7, 8, 9, 7]);
+            helper.insertionSort();
+            helper.removeDuplicates();
+            expect(helper.toString()).toBe('[1, 2, 3, 5, 7, 8, 9, 7, 8, 9]');
+        });
+    });
+
+    describe('Array of length 1', () => {
+        test('Return original array', () => {
+            let helper = new ArrayHelper([1]);
+            helper.removeDuplicates();
+
+            expect(helper.toString()).toBe('[1]');
+        })
+    });
+
+    describe('Array of length 0', () => {
+        test('Return original array', () => {
+            let helper = new ArrayHelper([]);
+            helper.removeDuplicates();
+
+            expect(helper.toString()).toBe('[]');
+        })
+    });
+
+    describe('Array with unique elements', () => {
+        test('Return original array', () => {
+            let helper = new ArrayHelper([1, 2, 3, 4, 5]);
+            helper.removeDuplicates();
+
+            expect(helper.toString()).toBe('[1, 2, 3, 4, 5]');
+        });
+    });
+});
+
 describe('#toString', () => {
     test('Returns a formatted string', () => {
         let helper = new ArrayHelper([1, 2, 3]);
