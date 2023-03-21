796f236ee943aed637dca74ea9d9479e1f01c74a
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-20 21:08:59 -0400

    Manual testing.

--- public/app.js
+++ public/app.js
@@ -6,15 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ArrayHelper_1 = require("./ArrayHelper");
 const BinarySearchTree_1 = require("./BinarySearchTree");
 //let inputArray = [5, 4, 3, 2, 1];
-let sorter = new ArrayHelper_1.ArrayHelper([6, 5, 4, 5, 9, 3, 2, 5, 10, 1]);
+let sorter = new ArrayHelper_1.ArrayHelper([6, 5, 4, 5, 9, 3, 2, 5, 1]);
 sorter.insertionSort();
 sorter.removeDuplicates();
 console.log(`Output: ${sorter.toString()}\n`);
 //let newArray = sorter.getArray();
 let bst = new BinarySearchTree_1.BinarySearchTree();
-let root = bst.getRootNode();
-root = bst.buildBST(sorter.getArray(), 0, sorter.getArray().length - 1);
+//let root = bst.getRootNode();
+let root = bst.buildBST(sorter.getArray(), 0, sorter.getArray().length - 1);
 console.log(bst.prettyPrint(root));
+bst.deleteNode(9);
+bst.deleteNode(1);
+console.log(`${bst.prettyPrint(root)}`);
 /*var array = [1,3,2,1,3,4,5,6,7,3,5,6,4,3]
 let output: number[] = new Array();
 
--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,20 @@
 import { ArrayHelper } from "./ArrayHelper";
 import { BinarySearchTree } from "./BinarySearchTree";
 //let inputArray = [5, 4, 3, 2, 1];
-let sorter = new ArrayHelper([6, 5, 4, 5, 9, 3, 2, 5, 10, 1]);
+let sorter = new ArrayHelper([6, 5, 4, 5, 9, 3, 2, 5, 1]);
 sorter.insertionSort();
 sorter.removeDuplicates();
 
 console.log(`Output: ${sorter.toString()}\n`);
 //let newArray = sorter.getArray();
 let bst = new BinarySearchTree();
-let root = bst.getRootNode();
-root = bst.buildBST(sorter.getArray(), 0, sorter.getArray().length-1)!;
+//let root = bst.getRootNode();
+let root = bst.buildBST(sorter.getArray(), 0, sorter.getArray().length-1)!;
 console.log(bst.prettyPrint(root));
 
+bst.deleteNode(9);
+bst.deleteNode(1);
+console.log(`${bst.prettyPrint(root)}`);
 /*var array = [1,3,2,1,3,4,5,6,7,3,5,6,4,3]
 let output: number[] = new Array();
 
