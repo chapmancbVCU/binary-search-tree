65156de6dff61064e22600982f2bf0633351e516
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-21 20:47:02 -0400

    Manual testing.

--- public/app.js
+++ public/app.js
@@ -4,8 +4,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * IMPORTS                                                                    *
  *****************************************************************************/
 const ArrayHelper_1 = require("./ArrayHelper");
+const BinarySearchTree_1 = require("./BinarySearchTree");
 //let inputArray = [5, 4, 3, 2, 1];
-let sorter = new ArrayHelper_1.ArrayHelper([1, 2, 3]);
-sorter.insertionSort();
+let sorter = new ArrayHelper_1.ArrayHelper([1, 2, 3, 4, 5]);
+//sorter.insertionSort();
 sorter.removeDuplicates();
-console.log(`Output: ${sorter.toString()}\n`);
+let array = sorter.getArray();
+let bst = new BinarySearchTree_1.BinarySearchTree();
+bst.buildBST(array, 0, array.length - 1);
+console.log(bst.prettyPrint(bst.getRootNode()));
+console.log(bst.toStringPostorder(bst.getRootNode()));
--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,11 @@
 import { ArrayHelper } from "./ArrayHelper";
 import { BinarySearchTree } from "./BinarySearchTree";
 //let inputArray = [5, 4, 3, 2, 1];
-let sorter = new ArrayHelper([1, 2, 3]);
-sorter.insertionSort();
+let sorter = new ArrayHelper([1, 2, 3, 4, 5]);
+//sorter.insertionSort();
 sorter.removeDuplicates();
-
-console.log(`Output: ${sorter.toString()}\n`);
+let array = sorter.getArray();
+let bst = new BinarySearchTree();
+bst.buildBST(array, 0, array.length - 1);
+console.log(bst.prettyPrint(bst.getRootNode()));
+console.log(bst.toStringPostorder(bst.getRootNode()));
